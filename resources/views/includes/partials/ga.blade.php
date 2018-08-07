b941eb74cf9838dd621ccad89a220dcbced5bbf1
ajayzend <ajay.kumar.iimt@gmail.com>
2018-08-07 22:39:35 +0530

    Commented Google Analytics code

--- resources/views/includes/partials/ga.blade.php
+++ resources/views/includes/partials/ga.blade.php
@@ -1,5 +1,8 @@
+{{--
 @if (config("analytics.google-analytics") && config("analytics.google-analytics") != "UA-XXXXX-X")
-    {{-- Google Analytics: change UA-XXXXX-X to be your site's ID. --}}
+    --}}
+{{-- Google Analytics: change UA-XXXXX-X to be your site's ID. --}}{{--
+
     <script>
         (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
                 function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
@@ -8,4 +11,4 @@
             r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
         ga('create','{{ config("analytics.google-analytics") }}','auto');ga('send','pageview');
     </script>
-@endif
+@endif--}}
