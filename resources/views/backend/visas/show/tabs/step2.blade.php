1a269c0b3057ed0519d5d07b1ff20326fb030a61
ajayzend <ajay.kumar.iimt@gmail.com>
2018-08-27 23:01:31 +0530

    Fields added in detail page

--- resources/views/backend/visas/show/tabs/step2.blade.php
+++ resources/views/backend/visas/show/tabs/step2.blade.php
@@ -1,7 +1,74 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: AJAY SAHU
- * Date: 11-08-2018
- * Time: 14:52
- */
+<table class="table table-striped table-hover">
+    <tr>
+        <th>Surname</th>
+        <td>{{ $visa->p1_lname }}</td>
+    </tr>
+
+    <tr>
+        <th>Middle Name</th>
+        <td>{{ $visa->p1_mname }}</td>
+    </tr>
+
+
+    <tr>
+        <th>Given Name/s</th>
+        <td>{{ $visa->p1_fname }}</td>
+    </tr>
+
+    <tr>
+        <th>Surname</th>
+        <td>{{ $visa->p2_previous_surname }}</td>
+    </tr>
+
+
+    <tr>
+        <th>Name</th>
+        <td>{{ $visa->p2_previous_name }}</td>
+    </tr>
+
+    <tr>
+        <th>Gender</th>
+        <td>{{ $visa->p2_gender }}</td>
+    </tr>
+
+    <tr>
+        <th>Date of Birth</th>
+        <td>{{ $visa->p1_dob }}</td>
+    </tr>
+
+    <tr>
+        <th>Temp</th>
+        <td>{{ $visa->visa_no }}</td>
+    </tr>
+
+    <tr>
+        <th>Temp</th>
+        <td>{{ $visa->visa_no }}</td>
+    </tr>
+
+    <tr>
+        <th>Temp</th>
+        <td>{{ $visa->visa_no }}</td>
+    </tr>
+
+    <tr>
+        <th>Temp</th>
+        <td>{{ $visa->visa_no }}</td>
+    </tr>
+
+    <tr>
+        <th>Temp</th>
+        <td>{{ $visa->visa_no }}</td>
+    </tr>
+
+    <tr>
+        <th>Temp</th>
+        <td>{{ $visa->visa_no }}</td>
+    </tr>
+
+    <tr>
+        <th>Temp</th>
+        <td>{{ $visa->visa_no }}</td>
+    </tr>
+
+</table>
