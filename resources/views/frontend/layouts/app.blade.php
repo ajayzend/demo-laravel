dfdd979d3dce640a696fd0cd51c0a6db376ebcd1
ajayzend <ajay.kumar.iimt@gmail.com>
2018-11-18 21:47:54 +0530

    Google verification code added for G-Suite business email

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -9,7 +9,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>@yield('title', app_name())</title>
-
+        <meta name="google-site-verification" content="80oFWWlwO9ySj29V2e4_L46FLzr1ceiC6TCFFjPHUPw" />
         <!-- Meta -->
         <meta name="description" content="@yield('meta_description', 'e-visa-India')">
         <meta name="author" content="@yield('meta_author', 'Ajay Sahu')">
