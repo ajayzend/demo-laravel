793604512a2e7fae9efb1315d15e09f68d481a0e
ajayzend <ajay.kumar.iimt@gmail.com>
2019-08-30 23:10:45 +0530

    custom page added for 404 error

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,59 +1,13 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>{{ trans('http.404.title') }}</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('frontend.layouts.app')
 
-    <style>
-        * {
-            line-height: 1.2;
-            margin: 0;
-        }
-
-        html {
-            color: #888;
-            display: table;
-            font-family: sans-serif;
-            height: 100%;
-            text-align: center;
-            width: 100%;
-        }
-
-        body {
-            display: table-cell;
-            vertical-align: middle;
-            margin: 2em auto;
-        }
-
-        h1 {
-            color: #555;
-            font-size: 2em;
-            font-weight: 400;
-        }
-
-        p {
-            margin: 0 auto;
-            width: 280px;
-        }
-
-        @media only screen and (max-width: 280px) {
-
-            body, p {
-                width: 95%;
-            }
-
-            h1 {
-                font-size: 1.5em;
-                margin: 0 0 0.3em;
-            }
-
-        }
-    </style>
-</head>
-<body>
-<h1>{{ trans('http.404.title') }}</h1>
-<p>{{ trans('http.404.description') }}</p>
-</body>
-</html>
-<!-- IE needs 512+ bytes: http://blogs.msdn.com/b/ieinternals/archive/2010/08/19/http-error-pages-in-internet-explorer.aspx -->
+@section('content')
+        <!-- Content -->
+<div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+    <div class="row">
+        <div class="about_us">
+            <h1>{{ trans('http.404.title') }}</h1>
+            <p>{{ trans('http.404.description') }}</p>
+        </div>
+    </div>
+</div>
+@endsection
