c1238952aed36ec287aa18e31a3b341e7a4ac953
ajayzend <ajay.kumar.iimt@gmail.com>
2019-08-30 23:00:26 +0530

    seo page commited

--- config/seo.php
+++ config/seo.php
@@ -3,61 +3,61 @@
 return [
 
     'home' => [
-        'title'     => "ApplyFor Indian EVisa Online | Indian Visa Application – Evisaindia.in",
-        'description' => "India Visa: Evisaindia.in is a leading Indian Visa website. Get assured instant visa for India to UK, India to US. Apply now for Indian e-visa application.",
-        'keywords'      => "indian e visa, india visa, indian visa, evisaindia, visa to indiauk, visa to india us, indian visa online, indian visa application, online indian visa application, indian e visa online"
+        'title'     => "Apply For Indian EVisa Online | Indian Visa Application – Evisaindia.in",
+        'description' => "India Visa: Evisaindia.in is a leading Indian Visa website. Get assured instant visa to India UK, visa to India US. Apply now for Indian e-visa application.",
+        'keywords'      => "indian e visa, india visa, indian visa, evisaindia, visa to india uk, visa to india us, indian visa online, indian visa application, online indian visa application, indian e visa online"
     ],
 
     'business-visa' => [
-        'title'     => "ApplyIndian E-Business Visa Online Services | E Business Visa India–Evisaindia.in",
+        'title'     => "Apply Indian E-Business Visa Online Services | E Business Visa India – Evisaindia.in",
         'description' => "India Business Visa: Those seeking for business-related trips to India making sales or within and out of the country. Visit leading Indian e visa website today.",
         'keywords'      => "india business visa,e business visa india,Indian e-Business Visa",
         'h1'      => "Apply for Indian e-business visa"
     ],
 
     'create' => [
-        'title'     => "Application For India Visa | ApplyIndian Visa From USA–Evisaindia.in",
+        'title'     => "Application For India Visa | Apply Indian Visa From USA – Evisaindia.in",
         'description' => "Apply Visa To India: Evisaindia.in provides visa for more than 180+ countries across the world such as UK, USA, Australia, UAE and so many. Apply Indian visa from USA.",
         'keywords'      => "apply for indian visa, apply visa to india, apply for indian visa from usa, apply for indian visa online, apply indian e visa online, indian visa application form",
-        'h1'      => "Apply for Indian E- Visa Online"
+        'h1'      => "Apply for Indian E-Visa Online"
     ],
 
     'medical-visa' => [
         'title'     => "Indian E-Medical Visa | Apply For Indian Medical Visa Online – Evisaindia.in",
         'description' => "India Medical Visa: Those seeking for medical treatment in Indian reputed specialized hospitals. Apply for e-medical visa India online today",
         'keywords'      => "e-medical visa india, medical visa india, India Medical Visa, apply for indian medical visa",
-        'h1'      => "Apply for medical e- visa online"
+        'h1'      => "Apply For Medical E-Visa Online"
     ],
 
     'tourist-visa' => [
-        'title'     => "Indian Tourist Visa Online| India Tourist E Visa –Evisaindia.in",
+        'title'     => "Indian Tourist Visa Online | India Tourist E Visa – Evisaindia.in",
         'description' => "India Travel E Visa: Evisaindia.in provides online applications for an Indian tourist e-visa. Now get online assistance, visa for goa and India travel e visa hassle free, start applying now!",
         'keywords'      => "visa for goa, indian tourist visa, e tourist visa india, india tourist e visa, india travel e visa",
-        'h1'      => "Apply For Tourist E- Visa Online"
+        'h1'      => "Apply For Tourist E-Visa Online"
     ],
 
     'urgent-visa' => [
         'title'     => "Urgent E- Visa India | Emergency Visa Application Services – Evisaindia.in",
         'description' => "Urgent e-Visa India online: Visit Evisaindia.in website today for hassle free processing. Apply for medical e-visa, business and e-tourist visa from anywhere across the globe.",
         'keywords'      => "Urgent e-tourist Visa",
-        'h1'      => "Apply For Urgent E- Visa Online"
+        'h1'      => "Apply For Urgent E-Visa Online"
     ],
 
     'document' => [
-        'title'     => "Document RequirementFor Indian E-Visa Online –Evisaindia.in",
+        'title'     => "Document Requirement For Indian E-Visa Online – Evisaindia.in",
         'description' => "EvisaIndia.in provides online services for India E-Visa applications also available for tourist, business & medical visits. Apply online today with all required document.",
         'keywords'      => "temp"
     ],
 
     'instruction' => [
-        'title'     => "Instructions ForIndian E-Visa Online Application–Evisaindia.in",
+        'title'     => "Instructions For Indian E-Visa Online Application – Evisaindia.in",
         'description' => "Indian E-Visa Online: Know the instructions for the online Indian visa application. Evisaindia.in provides the Indian E-Visa online in many countries like USA, UK.",
         'keywords'      => "temp"
     ],
 
     'faq' => [
-        'title'     => "Frequently Asked Questions About Indian Visa –Evisaindia.in",
-        'description' => "Apply Indian visa online: Evisaindia.in is a leading e-visa company. Apply for indian e visa from worldwide at less processing time and costing.",
+        'title'     => "Frequently Asked Questions About Indian Visa – Evisaindia.in",
+        'description' => "Apply Indian visa online: Evisaindia.in is a leading e-visa company in India. Apply for indian e visa from worldwide at less processing time and costing.",
         'keywords'      => "temp"
     ],
 
